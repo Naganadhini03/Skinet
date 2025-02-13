864511ca9f63625ea917ccc92fe0c083a021c85a
Naganandhini <naganandhiniak.kumaran@gmail.com>
2025-02-13 10:39:41 +0530

    end of today

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 // Add Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
@@ -40,4 +43,16 @@ app.UseAuthorization(); // Authorization middleware (if needed)
 
 app.MapControllers(); 
 
+try{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 app.Run(); // Listen on HTTP port
